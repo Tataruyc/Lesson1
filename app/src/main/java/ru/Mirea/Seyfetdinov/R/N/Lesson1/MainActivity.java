d6a97b17038c62228e3d251baf36a6acb94caadc
Пользователь <123rin12320@gmail.com>
2023-02-18 17:39:23 +0300

    init

---
+++ app/src/main/java/ru/Mirea/Seyfetdinov/R/N/Lesson1/MainActivity.java
@@ -0,0 +1,14 @@
+package ru.Mirea.Seyfetdinov.R.N.Lesson1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
